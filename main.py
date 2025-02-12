d8ec9bac9d6814ebcc09de204304852efc686dde
Alexey <practicemakesperfect73@gmail.com>
2025-02-12 15:04:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import requests
 
 
-def main(location):
-    url = f'https://wttr.in/{location}?nTqM&lang=ru'
-    response = requests.get(url)
-    response.raise_for_status()
-    print(response.text)
-
-
-if __name__ == "__main__":
+def main():
     locations = ['Лондон', 'Шереметьево', 'Череповец']
+    payload = {'n': '', 'T': '', 'q': '', 'M': '', 'lang': 'ru'}
     for location in locations:
-        main(location)
+        url = f'https://wttr.in/{location}'
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
+        print(response.text)
+
+
+if __name__ == '__main__':
+    main()
